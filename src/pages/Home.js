948f0f9536b660d949f36a149b6c20027d2b81eb
khamliche-jnAvzu <khamliche@gmail.com>
2023-02-06 10:36:27 +0000

    add homeandproject code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="myhome">
+        <h1>Tarik Khamliche</h1>
+        <h2> Front end Developer</h2>
+        <p>
+          As a tech-savvy developer with a passion for both learning and
+          creating, <br></br>I bring excitement to every project I work on
+          through my love of development and design
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
