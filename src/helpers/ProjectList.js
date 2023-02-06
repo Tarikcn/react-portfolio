6aef5bd90425a078630c74f85c9abea3058bf312
khamliche-jnAvzu <khamliche@gmail.com>
2023-02-06 14:59:08 +0000

    add contactpage-code

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,44 @@
+import Proj1 from "../assets/project1.JPG";
+import Proj2 from "../assets/project2.png";
+import Proj3 from "../assets/project3.JPG";
+import Proj4 from "../assets/project4.png";
+import Proj5 from "../assets/project5.JPG";
+import Proj6 from "../assets/project6.png";
+
+export const ProjectList = [
+  {
+    name: "Advanced Weather App",
+    image: Proj1,
+    skills: ["HTML", "CSS", "Javascript", "WeatherAPI", "Geolocation"],
+  },
+
+  {
+    name: "Todo List App",
+    image: Proj2,
+    skills: ["HTML", "CSS", "Javascript"],
+  },
+
+  {
+    name: "Security Company Website",
+    image: Proj3,
+    skills: ["HTML", "CSS", "Javascript", "Bootstrap", "JQuery"],
+  },
+
+  {
+    name: "Team Profile Generator",
+    image: Proj4,
+    skills: ["react", "Javascript", "NodeJS", "Inquirer"],
+  },
+
+  {
+    name: "ReadMe file Generator",
+    image: Proj5,
+    skills: ["Javascript", "NodeJS", "Inquirer"],
+  },
+
+  {
+    name: "Guess Number Game",
+    image: Proj6,
+    skills: ["HTML", "CSS", "Javascript", "Bootstrap", "JQuery"],
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ProjectItem from "../components/ProjectItem";
-import Proj1 from "../assets/project1.JPG";
-import Proj2 from "../assets/project2.png";
-import Proj3 from "../assets/project3.JPG";
-import Proj4 from "../assets/project4.png";
+import { ProjectList } from "../helpers/ProjectList";
 
 import "../styles/Projects.css";
+
 function Projects() {
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Advanced Weather App" image={Proj1} />
-        <ProjectItem name="Todo List App" image={Proj2} />
-        <ProjectItem name="Security Company Website" image={Proj3} />
-        <ProjectItem name="Team Profile generator" image={Proj4} />
+        {ProjectList.map((project, index) => {
+          return (
+            <Link to={`/project/${index}`} key={index}>
+              <ProjectItem name={project.name} image={project.image} />
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
