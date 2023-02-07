12f7da795df5191f73adc152afecadc751e3dd0e
khamliche-jnAvzu <khamliche@gmail.com>
2023-02-07 17:59:26 +0000

    projectrepolinks fixed

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -11,7 +11,7 @@ function ProjectDisplay() {
   return (
     <div className="project">
       <h1> {project.name} </h1>
-      <img src={project.image} />
+      <img src={project.image} alt="projectimages" />
       <p>
         <b>skills</b>: {project.skills.join(", ")}
       </p>
